1ae2afc99766493d65bac2efd962eff05ff1caf6
Marius Isken <marius.isken@gfz-potsdam.de>
2023-06-15 00:24:20 +0200

    phasenet: adding stack method

--- lassie/images/phase_net.py
+++ lassie/images/phase_net.py
@@ -88,7 +88,7 @@ class PhaseNet(ImageFunction):
     window_overlap_samples: conint(ge=1000, le=3000) = 2000
     torch_use_cuda: bool = False
     torch_cpu_threads: PositiveInt = 4
-    stack_method: StackMethod = "max"
+    stack_method: StackMethod = "avg"
     phase_map: dict[PhaseName, str] = {
         "P": "constant:P",
         "S": "constant:S",
