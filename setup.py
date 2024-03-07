bbccae388ea6419f12fd7721f50d8f2dcc07fe11
Marius Isken <marius.isken@gfz-potsdam.de>
2024-03-07 18:54:05 +0100

    CI/CD update

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ from setuptools import Extension, setup
 
 is_mac = "macos" in sysconfig.get_platform().lower()
 
-extra_link_args = ["-lomp"] if is_mac else ["-lgomp"]
+
+extra_compile_args = ["-fopenmp"]
+extra_link_args = ["-lgomp"]
+if is_mac:
+    extra_compile_args = ["-fopenmp=libomp"]
+    extra_link_args = ["-lomp"]
 
 setup(
     ext_modules=[
@@ -14,7 +19,7 @@ setup(
             "qseek.ext.array_tools",
             sources=["src/qseek/ext/array_tools.c"],
             include_dirs=[numpy.get_include()],
-            extra_compile_args=["-fopenmp", "-O3", "-flto"],
+            extra_compile_args=["-O3", "-flto", *extra_compile_args],
             extra_link_args=extra_link_args,
         )
     ]
