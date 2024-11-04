2b288acbca564f80d45fd5f6f1a8a3a4ee08e563
miili <marius.isken@gmail.com>
2024-11-04 16:13:34 +0100

    stations: better info log

--- src/qseek/waveforms/base.py
+++ src/qseek/waveforms/base.py
@@ -66,7 +66,7 @@ class WaveformBatch:
             bool: True if the batch is empty, False otherwise.
         """
         if self.n_stations < min_stations:
-            logger.warning("batch has less than 3 stations")
+            logger.warning("batch has less than only %d stations", self.n_stations)
             return False
         if not self.traces:
             logger.warning("batch is empty")
