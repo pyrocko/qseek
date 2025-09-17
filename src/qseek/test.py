6f3077d2e56790a3a8477a028cc508356d47afd2
marius <marius.isken@gfz-potsdam.de>
2025-09-17 18:31:23 +0200

    cleaning unused files

--- src/qseek/test.py
+++
@@ -1,7 +0,0 @@
-from qseek import plot
-from qseek.models.catalog import EventCatalog
-
-detections = EventCatalog(rundir="test-qseek/")
-detection = detections.events[0]
-
-plot.plot_detection(detection)
