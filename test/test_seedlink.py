ff372723cd0c7c99ec403e0bda72ffc9e3206194
marius <marius.isken@gfz-potsdam.de>
2025-09-30 18:37:48 +0200

    seedlink: fixing test

--- test/test_seedlink.py
+++ test/test_seedlink.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib
 import logging
 from datetime import timedelta
 
@@ -6,7 +7,7 @@ import pytest
 
 from qseek.utils import _NSL, datetime_now
 from qseek.waveforms.seedlink.client import SeedLinkClient, StationSelection
-from qseek.waveforms.seedlink.seedlink import SeedLink
+from qseek.waveforms.seedlink.seedlink import SeedLink, slinktool_available
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -51,6 +52,7 @@ async def seedlink_client():
 
     client.start_streams()
 
+    i_batch = 0
     while True:
         await asyncio.sleep(0)
         start = datetime_now()
@@ -68,44 +70,59 @@ async def seedlink_client():
             return_exceptions=True,
         )
         assert traces
+        i_batch += 1
+        if i_batch >= 3:
+            break
 
 
+@pytest.mark.skipif(not slinktool_available(), reason="slinktool not available")
 @pytest.mark.asyncio
 async def test_seedlink():
     seedlink = get_seedlink_client()
-    i_batch = 0
 
-    async for batch in seedlink.iter_batches(
-        window_increment=timedelta(seconds=5),
-        window_padding=timedelta(seconds=5),
-        start_time=datetime_now(),
-        min_length=timedelta(seconds=5),
-        min_stations=1,
-    ):
-        assert batch.traces
-        # print(f"batch {i_batch} with {len(batch.traces)} traces")
-        i_batch += 1
-        if i_batch >= 3:
-            break
+    received_traces = 0
+
+    async def get_batches():
+        nonlocal received_traces
+        async for batch in seedlink.iter_batches(
+            window_increment=timedelta(seconds=5),
+            window_padding=timedelta(seconds=5),
+            start_time=datetime_now(),
+            min_length=timedelta(seconds=5),
+            min_stations=1,
+        ):
+            assert batch.traces
+            received_traces += len(batch.traces)
+
+    with contextlib.suppress(asyncio.TimeoutError):
+        await asyncio.wait_for(get_batches(), timeout=30.0)
+
+    assert received_traces > 0
 
 
+@pytest.mark.skipif(not slinktool_available(), reason="slinktool not available")
 @pytest.mark.asyncio
 async def test_seedlink_past():
     seedlink = get_seedlink_client()
-    i_batch = 0
 
-    async for batch in seedlink.iter_batches(
-        window_increment=timedelta(seconds=5),
-        window_padding=timedelta(seconds=5),
-        start_time=datetime_now() - timedelta(days=2),
-        min_length=timedelta(seconds=5),
-        min_stations=1,
-    ):
-        assert batch.traces
-        # print(f"batch {i_batch} with {len(batch.traces)} traces {batch.start_time}")
-        i_batch += 1
-        if i_batch >= 3:
-            break
+    received_traces = 0
+
+    async def get_batches():
+        nonlocal received_traces
+
+        async for batch in seedlink.iter_batches(
+            window_increment=timedelta(seconds=5),
+            window_padding=timedelta(seconds=5),
+            start_time=datetime_now() - timedelta(days=2),
+            min_length=timedelta(seconds=5),
+            min_stations=1,
+        ):
+            assert batch.traces
+            received_traces += len(batch.traces)
+
+    with contextlib.suppress(asyncio.TimeoutError):
+        await asyncio.wait_for(get_batches(), timeout=30.0)
+    assert received_traces > 0
 
 
 if __name__ == "__main__":
