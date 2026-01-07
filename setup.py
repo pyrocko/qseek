abc77fe2b1eec0e2ccdc4f84d75b4e8aa29151d8
miili <marius.isken@gmail.com>
2026-01-07 11:28:56 +0100

    delay sum: Adding new stacking module

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import numpy
+import simde_py
 from setuptools import Extension, setup
 
 setup(
@@ -9,6 +10,18 @@ setup(
             sources=["src/qseek/ext/array_tools.c"],
             include_dirs=[numpy.get_include()],
             extra_compile_args=["-fopenmp", "-O3", "-flto"],
+            extra_link_args=["-lgomp"],
+        ),
+        Extension(
+            "qseek.ext.delay_sum",
+            sources=["src/qseek/ext/delay_sum.c"],
+            include_dirs=[numpy.get_include(), simde_py.get_include()],
+            extra_compile_args=[
+                "-fopenmp",
+                "-O3",
+                "-flto",
+                "-march=native",
+            ],
             extra_link_args=["-lgomp"],
         ),
     ]
