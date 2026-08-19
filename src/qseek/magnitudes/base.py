4ffbcea38386357d42eb86e489af0ec04c2e1b42
AliciaRoh <nero.ro@web.de>
2026-08-19 09:58:37 +0200

    correcting formatting

--- src/qseek/magnitudes/base.py
+++ src/qseek/magnitudes/base.py
@@ -247,10 +247,8 @@ class StationAmplitudes(NamedTuple):
             peak_amp=float(peak_amp),
             noise=float(noise_amp),
             noise_std=float(noise_std),
-            distance_hypo=(
-                receiver.distance_to(event)
-                if not station_depth_only
-                else hypo_distance_only_station_depth(receiver, event)
-            ),
+            distance_hypo=receiver.distance_to(event)
+            if not station_depth_only
+            else hypo_distance_only_station_depth(receiver, event),
             distance_epi=receiver.surface_distance_to(event),
         )
